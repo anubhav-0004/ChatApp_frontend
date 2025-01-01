09f2128561df44986f44ec53f5ec7df87f489f02
anubhav-0004 <2003anubhav@gmail.com>
2025-01-01 09:08:27 +0530

    Input for messages added

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import AppLayout from "../components/layout/AppLayout";
+import { IoMdAttach } from "react-icons/io";
+import { IoSendSharp } from "react-icons/io5";
+import { LuSendHorizontal } from "react-icons/lu";
 
 const Chat = () => {
   return (
-    <div
-      className="text-[#dfd3ad] w-full h-full text-xl opacity-95 p-2 overflow-y-auto"
-      style={{
-        backgroundImage: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW0GqVqurY1V77t3jqPKUVYKRXa_HzZ4BBkw&s")`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "bottom",
-        backgroundAttachment: "fixed",
-      }}
-    >
-      <Chatss />
-      <Chatss />
-      <Chatss />
-      <Chatss />
-      <Chatss />
-      <Chatss />
-      <Chatss />
+    <div className="w-full h-full max-sm:h-[99%] flex flex-col">
+      <div
+        className="text-[#dfd3ad] w-full h-[92%] text-xl opacity-95 p-2 overflow-y-auto"
+        style={{
+          backgroundImage: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW0GqVqurY1V77t3jqPKUVYKRXa_HzZ4BBkw&s")`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "bottom",
+          backgroundAttachment: "fixed",
+        }}
+      >
+        <Chatss />
+        <Chatss />
+        <Chatss />
+        <Chatss />
+        <Chatss />
+        <Chatss />
+        <Chatss />
+      </div>
+      <form  className="flex items-center relative justify-between border border-[#6d6d75] gap-x-3 px-2 py-1 bg-[#646471] h-[8%]">
+        <IoMdAttach className="text-[#e9e0f9] text-2xl absolute cursor-pointer left-4 mb-1 bg-[#5c5c8a] rotate-[45deg] border border-slate-400 p-[.4rem] w-10 h-10 rounded-[50%]" />
+        <input type="text" placeholder="Type a message" className="flex flex-grow border-none outline-none px-16 text-white bg-transparent rounded-md h-[80%]"/>
+        <LuSendHorizontal type="submit" className="text-2xl text-[#e9e0f9] cursor-pointer absolute right-4 bg-[#5c5c8a] border border-slate-400 p-[.4rem] w-10 h-10 rounded-[50%] -rotate-[35deg] mb-1" />
+      </form>
     </div>
   );
 };
